7e140d90f6b97e98cfd19d07868f564810e12535
Antony Nganga <antonynganga05@gmail.com>
2019-03-23 17:09:30 +0300

    Display of the list of quotes in the parent component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -13,7 +13,7 @@ export class QuotesComponent implements OnInit {
     new Quotes(3,'He was a nobody. Yet and still, he held the gun and therfore all of the power.','Brima Lamin & Chantale Wesley-Lamin', new Date(2019,3,21)),
     new Quotes(4,'Nobody can do you better than you do you.','Kayambila Mpulamasaka', new Date(2019,3,22)),
     new Quotes(5,'Among the Igbo the art of conversation is regarded very highly, and proverbs are the palm-oil with which words are eaten.',' Chinua Achebe', new Date(2019,3,23)),
-    new Quotes(6,'Our people think: I , Wangari, a Kenyan by birth - how can I be a vagrant in my own country as if I were a foreigner.',"Ngũgĩ wa Thiong'o", new Date(2019,3,22))
+    new Quotes(6,'Our people think: I , Wangari, a Kenyan by birth - how can I be a vagrant in my own country as if I were a foreigner.',"Ngũgĩ wa Thiong'o", new Date(2019,3,22)),
   ]
 
   constructor() { }
