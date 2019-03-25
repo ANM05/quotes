7fbdf8bd2830010b22eca96c88f09347e4226f06
Antony Nganga <antonynganga05@gmail.com>
2019-03-25 10:15:29 +0300

    Adds submit function to the form button

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
   public upvotes:number;
   public downvotes:number;
-  constructor(public id:number, public quote:string, public author:string, public datePosted:Date){
+  constructor(public id:number, public quote:string, public author:string, public contName:string, public datePosted:Date){
     this.upvotes = 0;
     this.downvotes = 0;
   }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,12 +8,12 @@ import {Quotes} from '../quotes'
 })
 export class QuotesComponent implements OnInit {
   misemo = [
-    new Quotes(1,'Dont blow off anothers candle for it wont make yours shine brighter.','Jaachynma  Agu', new Date(2019,3,22)),
-    new Quotes(2,'In a world where everyone desperately tries to be different; be unapologetically yourself.','Kayambila Mpulamasaka', new Date(2019,3,20)),
-    new Quotes(3,'He was a nobody. Yet and still, he held the gun and therfore all of the power.','Brima Lamin & Chantale Wesley-Lamin', new Date(2019,3,21)),
-    new Quotes(4,'Nobody can do you better than you do you.','Kayambila Mpulamasaka', new Date(2019,3,22)),
-    new Quotes(5,'Among the Igbo the art of conversation is regarded very highly, and proverbs are the palm-oil with which words are eaten.',' Chinua Achebe', new Date(2019,3,23)),
-    new Quotes(6,'Our people think: I , Wangari, a Kenyan by birth - how can I be a vagrant in my own country as if I were a foreigner.',"Ngũgĩ wa Thiong'o", new Date(2019,3,22)),
+    new Quotes(1,'Dont blow off anothers candle for it wont make yours shine brighter.','Jaachynma  Agu','Jay', new Date(2019,3,22)),
+    new Quotes(2,'In a world where everyone desperately tries to be different; be unapologetically yourself.','Kayambila Mpulamasaka','Vee', new Date(2019,3,20)),
+    new Quotes(3,'He was a nobody. Yet and still, he held the gun and therfore all of the power.','Brima Lamin & Chantale Wesley-Lamin','Ras', new Date(2019,3,21)),
+    new Quotes(4,'Nobody can do you better than you do you.','Kayambila Mpulamasaka','Sharon', new Date(2019,3,22)),
+    new Quotes(5,'Among the Igbo the art of conversation is regarded very highly, and proverbs are the palm-oil with which words are eaten.',' Chinua Achebe','Shadra', new Date(2019,3,23)),
+    new Quotes(6,'Our people think: I , Wangari, a Kenyan by birth - how can I be a vagrant in my own country as if I were a foreigner.',"Ngũgĩ wa Thiong'o",'Dave', new Date(2019,3,22)),
   ]
   upvotes(i){
     this.misemo[i].upvotes+=1;
